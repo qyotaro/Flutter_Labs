e11a4818272777cedec69c3741260baa94ba23a3
Rostyslav <yotaroshindziro@gmail.com>
2024-11-21 18:09:47 +0200

    (lab5) Added a page using the API

--- lib/pages/recommend_page.dart
+++ lib/pages/recommend_page.dart
@@ -1,48 +1,52 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_project/utils/responsive_config.dart'; // Додано імпорт для адаптивності
+import 'package:flutter_project/utils/responsive_config.dart'; 
 import 'package:http/http.dart' as http;
 
 class RecommendedBookPage extends StatelessWidget {
   const RecommendedBookPage({super.key});
 
   Future<List<Map<String, String>>> fetchWantToReadBooks() async {
-    const String apiUrl = 'https://openlibrary.org/people/mekBot/books/want-to-read.json';
+  const String apiUrl = 'https://openlibrary.org/people/mekBot/books/want-to-read.json';
 
-    try {
-      final response = await http.get(Uri.parse(apiUrl));
+  try {
+    final response = await http.get(Uri.parse(apiUrl));
 
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> data = json.decode(response.body) as Map<String, dynamic>;
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body)
+       as Map<String, dynamic>;
 
-        final List<dynamic>? entries = data['reading_log_entries'] as List<dynamic>?;
-        if (entries == null || entries.isEmpty) {
-          return [];
-        }
-
-        return entries.map<Map<String, String>>((entry) { // Додаємо явне зазначення типу для колекції
-          final Map<String, dynamic> work = (entry['work'] ?? {}) as Map<String, dynamic>;
-          return {
-            'title': work['title'] is String ? work['title'] as String : 'Unknown Title',
-            'author': work['author_names'] is List && (work['author_names'] as List).isNotEmpty
-                ? (work['author_names'] as List)[0] as String
-                : 'Unknown Author',
-          };
-        }).toList();
-      } else {
-        throw Exception('Failed to load want-to-read books');
+      final List<dynamic>? entries = data['reading_log_entries'] 
+      as List<dynamic>?;
+      if (entries == null || entries.isEmpty) {
+        return [];
       }
-    } catch (e) {
-      print('Error fetching books: $e');
-      return [];
+
+      return entries.map<Map<String, String>>((entry) { 
+        final Map<String, dynamic> work = (entry['work'] ?? <String, dynamic>{})
+        as Map<String, dynamic>;
+        return <String, String>{
+          'title': work['title'] is String ? work['title'] as String :
+           'Unknown Title',
+          'author': work['author_names'] is List 
+          && (work['author_names'] as List).isNotEmpty
+              ? (work['author_names'] as List)[0] as String
+              : 'Unknown Author',
+        };
+      }).toList();
+    } else {
+      throw Exception('Failed to load want-to-read books');
     }
+  } catch (e) {
+    return [];
   }
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Want to Read Books'),
+        title: const Text('Recommended books'),
       ),
       body: FutureBuilder<List<Map<String, String>>>( 
         future: fetchWantToReadBooks(),
@@ -61,7 +65,8 @@ class RecommendedBookPage extends StatelessWidget {
                   title: Text(
                     book['title']!,
                     style: TextStyle(
-                      fontSize: ResponsiveConfig.contentFontSize(context) / 1.2,  
+                      fontSize: ResponsiveConfig.contentFontSize(context) 
+                      / 1.2,  
                       fontWeight: FontWeight.bold,
                       color: const Color.fromARGB(255, 17, 20, 57),  
                     ),
@@ -81,7 +86,7 @@ class RecommendedBookPage extends StatelessWidget {
           }
         },
       ),
-      backgroundColor: const Color.fromARGB(255, 233, 241, 250),  // Колір фону
+      backgroundColor: const Color.fromARGB(255, 233, 241, 250),  
     );
   }
 }
